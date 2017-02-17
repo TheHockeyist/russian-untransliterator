ae125b169ae92cec8e0eda5b224fc76c096e4560
TheHockeyist <thockeyist@gmail.com>
2017-02-16 18:19:23 -0600

    Repeat for entire alphabet.
    Next will be the rules for Cyrillic letters next to each other and modifying them to get the correct result, asking the user for help in ambiguous cases if needed.

--- code.py
+++ code.py
@@ -5,7 +5,37 @@ cyrillic_string = raw_input
 
 # Mechanism to search the string and replace letters with Cyrillic equivalents, e.g. d to д. For unclear situations (e.g. deciding between и and й), it will either make an educated guess based on the context or ask the user what Cyrillic letter was actually meant.
 
-str.replace(d, д)
+str.replace("a", "а")
+str.replace("b", "б")
+str.replace("c", "ц")
+str.replace("č", "ч")
+str.replace("d", "д")
+str.replace("е", "е")
+str.replace("f", "ф")
+str.replace("g", "г")
+str.replace("h", "х")
+str.replace("i", "и")
+str.replace("j", "й")
+str.replace("k", "к")
+str.replace("l", "л")
+str.replace("m", "м")
+str.replace("n", "н")
+str.replace("o", "о")
+str.replace("p", "п")
+str.replace("q", "я")
+str.replace("r", "р")
+str.replace("s", "с")
+str.replace("š", "ш")
+str.replace("t", "т")
+str.replace("u", "у")
+str.replace("v", "в")
+str.replace("w", "в")
+str.replace("x", "х")
+str.replace("y", "ы")
+str.replace("z", "з")
+str.replace("ž", "ж")
+str.replace("'", "ь")
+str.replace(""", "ъ")
 
 # Check the Cyrillic transliteration for errors that break Russian orthography rules and fix them, e.g. цх ---> ч or йу ---> ю.
 
