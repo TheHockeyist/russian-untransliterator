71e192a6fae8113e57ad4784464daa64240de580
TheHockeyist <thockeyist@gmail.com>
2017-02-19 14:27:13 -0600

    Comment.
    Need. Code. Fix.

--- code.py
+++ code.py
@@ -75,7 +75,7 @@ cyrillic_string = cyrillic_string.replace("йо", "ё")
 
 # Corrects spelling of words when э should be used instead of е.
 
-cyrillic_string = cyrillic_string.replace("ето", "это")
+cyrillic_string = cyrillic_string.replace("ето", "это") # What about words like лето?
 cyrillic_string = cyrillic_string.replace("ети", "эти")
 
 # Corrects spelling of words like район, майор, and cases with йо.
